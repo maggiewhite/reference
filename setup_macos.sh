3b44a5aa2d30d2c634d894657a6e978f6211b977
Maggie White <maggie@whisper.ai>
2021-02-05 22:32:44 -0800

    move dotfiles to a dotfiles directory

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,5 +1,12 @@
 # note: assumes that you're running all of this from the root directory of this repo
 
+function append_once() {
+    if ! grep -q "$1" "$2" 2>/dev/null
+    then
+        echo -e "$1" >> "$2"
+    fi
+}
+
 # install xcode command line tools (does not include xcode)
 xcode-select --install
 
@@ -12,10 +19,17 @@ brew install --cask iterm2
 # setup git completion, needs brew packages to be installed first
 ln -s /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.zsh /usr/local/etc/bash_completion.d/
 
+# Install adb
+wget https://dl.google.com/android/repository/platform-tools-latest-darwin.zip -O platform_tools.zip && \
+    unzip -q platform_tools.zip && \
+    rm platform_tools.zip
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "source $PWD/.zprofile" > ~/.zprofile
-ln -s .zshrc ~/.zshrc
 wget http://cscope.sourceforge.net/cscope_maps.vim ~/.vim/
+append_once "source $PWD/dotfiles/.zprofile" ~/.zprofile
+ln -s dotfiles/.zshrc ~/.zshrc
+
+# TODO: add git config
 
 echo "Consider installing the following:
  - BetterSnapTool
